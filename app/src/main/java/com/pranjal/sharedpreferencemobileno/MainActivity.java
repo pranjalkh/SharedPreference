dd83e5b8de6e2c27cb780c57e20ed474c8ab08fe
pranjalkhandelwal <pranjalkhandelwal123@gmail.com>
2020-01-24 00:50:26 +0530

    Shared Preference

--- app/src/main/java/com/pranjal/sharedpreferencemobileno/MainActivity.java
+++ app/src/main/java/com/pranjal/sharedpreferencemobileno/MainActivity.java
@@ -12,55 +12,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String SHARED_PREFS="shared_prefs";
-    public static final String TEXT="text";
-    private EditText mobileno;
-    private EditText edshowall;
-    private Button save;
-    private Button showall;
-    private String text="";
-    int count=0;
+    public static final String SHARED_PREFS = "shared_prefs";
+    public static final String TEXT = "text";
+    private EditText meditTextMobileNumber;
+    private TextView mTextResult;
+    private Button mSave;
+    private Button mShowall;
+    private String text = "";
+    int count = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mobileno=findViewById(R.id.edmobile);
-        edshowall=findViewById(R.id.edshow);
-        save=findViewById(R.id.btn_save);
-        showall=findViewById(R.id.btn_showall);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPreferences=getSharedPreferences("phone",MODE_PRIVATE);
-                SharedPreferences.Editor editor= sharedPreferences.edit();
+        meditTextMobileNumber = findViewById(R.id.edmobile);
+        mTextResult = findViewById(R.id.edshow);
+        mSave = findViewById(R.id.btn_save);
+        mShowall = findViewById(R.id.btn_showall);
 
-                if(count>0)
-                editor.putString(" ",text+","+mobileno.getText().toString());
-                else
-                    editor.putString("",mobileno.getText().toString());
-                editor.commit();
-                count++;
-
-                Toast t=Toast.makeText(MainActivity.this,"done",Toast.LENGTH_LONG);
-                t.show();
-
-            }
-        });
-        showall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences("phone", MODE_PRIVATE);
-
-                text = sharedPreferences.getString(" ", "");
-                String s = text.replace(","," ");
-
-                edshowall.setText(s);
-                    //edshowall.setText("\n");
-
-
-            }
-        });
     }
 
+    public void Save(View v) {
+        SharedPreferences sharedPreferences = getSharedPreferences("NumberFile", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Mobile", text + meditTextMobileNumber.getText().toString() + ",");
+        editor.commit();
 
+    }
 
+    public void ShowAll(View v) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("NumberFile", MODE_PRIVATE);
+        text = sharedPreferences.getString("Mobile", "");
+        text = text.replace(",", "\n");
+        mTextResult.setText(text);
+
+    }
 }
